a963617e6baf1db5f7679a2ebebfdfce72291b9c
rosa0922 <rohit.savalagi@netcracker.com>
2025-09-08 19:35:02 +0530

    initial commit

--- src/app/components/todo/todo.ts
+++ src/app/components/todo/todo.ts
@@ -11,58 +11,72 @@ import { TodoServiceTs } from '../../services/todo';
 import { Todo } from '../../models/todo';
 
 @Component({
-  selector: 'app-todo',
-  imports: [
-    MatButton, MatSuffix, MatIconButton,
-    MatIcon,MatInput, ReactiveFormsModule, MatFormField, CommonModule ,MatError, FormsModule],
-  templateUrl: './todo.html',
-  styleUrl: './todo.less',
-  animations: [
-    trigger('openClose', [
-      state('closed', style({
-        height: '0px',
-        opacity: 0,
-        overflow: 'hidden'
-      })),
-      state('open', style({
-        height: '*',
-        opacity: 1,
-        overflow: 'hidden'
-      })),
-      transition('closed <=> open', [
-        animate('300ms ease-in-out')
-      ]),
-    ]),
-  ]
+    selector: 'app-todo',
+    imports: [
+        MatButton,
+        MatSuffix,
+        MatIconButton,
+        MatIcon,
+        MatInput,
+        ReactiveFormsModule,
+        MatFormField,
+        CommonModule,
+        MatError,
+        FormsModule,
+    ],
+    templateUrl: './todo.html',
+    styleUrl: './todo.less',
+    animations: [
+        trigger('openClose', [
+            state(
+                'closed',
+                style({
+                    height: '0px',
+                    opacity: 0,
+                    overflow: 'hidden',
+                })
+            ),
+            state(
+                'open',
+                style({
+                    height: '*',
+                    opacity: 1,
+                    overflow: 'hidden',
+                })
+            ),
+            transition('closed <=> open', [animate('300ms ease-in-out')]),
+        ]),
+    ],
 })
 export class TodoComponent {
-  isFormOpen = false;
-  formControl = new FormControl('', [Validators.required]);
-  matcher = new ShowOnDirtyErrorStateMatcher();
-  todoSerivce = inject(TodoServiceTs);
+    isFormOpen = false;
+    formControl = new FormControl('', [Validators.required]);
+    matcher = new ShowOnDirtyErrorStateMatcher();
+    todoService = inject(TodoServiceTs);
 
-
-  toggleForm() {
-    this.formControl.reset();
-    this.isFormOpen = !this.isFormOpen;
-  }
-
-  addTodo() {
-    if (this.formControl.valid) {
-      this.todoSerivce.postTodo({
-        title: this.formControl.value!,
-        completed: false
-      }).subscribe({
-        next: (todo: Todo) => {
-          console.log('Todo added:', todo);
-          this.formControl.reset();
-          this.formControl.markAsPristine();
-          this.formControl.markAsUntouched();
-        },
-        error: (err) => {
-          console.error('Error adding todo:', err);
-        }
-      });
+    toggleForm() {
+        this.formControl.reset();
+        this.isFormOpen = !this.isFormOpen;
     }
-  }
+
+    addTodo() {
+        if (this.formControl.valid) {
+            this.todoService
+                .postTodo({
+                    title: this.formControl.value!,
+                    completed: false,
+                })
+                .subscribe({
+                    next: (todo: Todo) => {
+                        console.log('Todo added:', todo);
+                        this.formControl.reset();
+                        this.formControl.markAsPristine();
+                        this.formControl.markAsUntouched();
+                    },
+                    error: err => {
+                        console.error('Error adding todo:', err);
+                    },
+                });
+        }
+    }
 }
